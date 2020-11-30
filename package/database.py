13865f7cc395182543a19da824a82f4f21b089e7
YS Kumar <70428965+YSKUMAR775@users.noreply.github.com>
2020-11-30 23:12:23 +0530

    Update database.py

--- package/database.py
+++ package/database.py
@@ -2,7 +2,7 @@ import pymysql
 
 
 def db_connect():
-    db = pymysql.connect(host='localhost',
+    db = pymysql.connect(host='aws2.c42ojr1a1cpj.ap-south-1.rds.amazonaws.com',
                          user='root',
                          password='S@i30051995',
                          db='voting_rds'
